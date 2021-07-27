065c79b794143d2b1d5ad52092804ac3c838f0fe
Ty <ltgray579@gmail.com>
2021-07-26 22:22:00 -0700

    Setting things up

---
+++ discord-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets Build a Discord Clone!!</h1>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Chat */}
+    </div>
+  );
+}
+
+export default App;
